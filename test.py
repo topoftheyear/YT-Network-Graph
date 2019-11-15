d93d90316d63c77e74a6ca372cf4b87421c1de63
topoftheyear <topoftheyear@Gmail.com>
2019-11-14 18:00:46 -0600

    Reworks the test for the branding settings

--- test.py
+++ test.py
@@ -6,28 +6,21 @@
 
 import os
 
-import google_auth_oauthlib.flow
 import googleapiclient.discovery
 import googleapiclient.errors
 
-scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
-
 def main():
-    # Disable OAuthlib's HTTPS verification when running locally.
-    # *DO NOT* leave this option enabled in production.
-    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
-
     api_service_name = "youtube"
     api_version = "v3"
     client_secrets_file = "API_key"
     api_key = open(client_secrets_file, 'r').read()
 
-    youtube = googleapiclient.discovery.build(
-        api_service_name, api_version, developerKey=api_key)
+    youtube = googleapiclient.discovery.build(api_service_name, api_version, developerKey=api_key)
 
     request = youtube.channels().list(
-        part="snippet,contentDetails,statistics",
-        id="UC_x5XG1OV2P6uZZ5FSM9Ttw"
+        part="brandingSettings",
+        #id="UC_x5XG1OV2P6uZZ5FSM9Ttw"
+        forUsername="GoogleDevelopers"
     )
     response = request.execute()
 
