9c6918475edddcf1d531cf424ca7d07d1e6c8fb3
topoftheyear <topoftheyear@Gmail.com>
2019-11-18 18:06:28 -0600

    Testing some visualization formats

---
+++ build.py
@@ -0,0 +1,32 @@
+import json
+
+from tqdm import tqdm
+
+def main():
+    channels = dict()
+    with open('all_channels.json', 'r') as f:
+        channels = json.load(f)
+        
+    table_format = ['channel_id\tname\tweight']
+    
+    print('Building nodes list')
+    for channel_id, info in tqdm(channels.items()):
+        name = str(info['name'].encode('utf-8'))
+        name = name[2:len(name) - 1]
+        weight = 0
+        for other_id, other_info in channels.items():
+            if other_id == channel_id:
+                continue
+                
+            if channel_id in other_info['connected_channels']:
+                weight += 1
+                
+        table_format.append(f'{channel_id}\t{name}\t{weight}')
+        
+    with open('nodes.txt', 'w') as f:
+        for element in table_format:
+            f.write(element)
+            f.write('\n')
+    
+if __name__ == "__main__":
+    main()
