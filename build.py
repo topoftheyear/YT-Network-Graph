85ba0ec5ab6a3940c7644bc670704addbda2d0ae
topoftheyear <topoftheyear@Gmail.com>
2019-11-19 14:48:38 -0600

    Add a bit more build testing

--- build.py
+++ build.py
@@ -20,13 +20,27 @@ def main():
                 
             if channel_id in other_info['connected_channels']:
                 weight += 1
-                
+        
         table_format.append(f'{channel_id}\t{name}\t{weight}')
         
     with open('nodes.txt', 'w') as f:
         for element in table_format:
             f.write(element)
             f.write('\n')
+            
+    edge_format = ['source\ttarget\tinteraction\tdirected']
+    
+    print('Building edge list')
+    for channel_id, info in tqdm(channels.items()):
+        for other in info['connected_channels']:
+            if other in channels.keys():
+                edge_format.append(f'{channel_id}\t{other}\tpp\ttrue')
+                
+    with open('edges.txt', 'w') as f:
+        for element in edge_format:
+            f.write(element)
+            f.write('\n')
+        
     
 if __name__ == "__main__":
     main()
